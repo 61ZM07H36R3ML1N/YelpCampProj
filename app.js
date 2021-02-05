74f847d6711d8bc0e749a29ce675ed0cc764a10d
BT <brianthomasjamiel@gmail.com>
2021-02-04 17:21:58 -0800

    basic server setup

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from yelpcamp!!!')
+})
+
+
+
+app.listen(3001, ()=> {
+    console.log('Serving on port 3001')
+})
