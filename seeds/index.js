b86b767b23edadeacaeb2e1fc108606b23cc3ff0
BT <brianthomasjamiel@gmail.com>
2021-02-07 14:59:52 -0800

    added const place and descriptor and await seedDB on bottom

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/YelpCampProj', {
@@ -25,4 +26,4 @@ const seedDB = async () => {
     }
 }
 
-seedDB();
+ await seedDB();
