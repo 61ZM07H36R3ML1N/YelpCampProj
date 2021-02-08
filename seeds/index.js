6c998eab6d80f0df99d70668c567fec3ef0a38a7
BT <brianthomasjamiel@gmail.com>
2021-02-07 21:08:22 -0800

    added code to flow for seed

--- seeds/index.js
+++ seeds/index.js
@@ -16,14 +16,17 @@ db.once("open", () => {
 });
 
 const seedDB = async () => {
+    console.log('seeding DB');
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++){
+    for(let i = 0; i < 100; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: '${cities[random1000].city}, ${cities[random1000].state}'
         })
         await camp.save();
+        console.log(`Record ${i} created`);
     }
+    db.close()
 }
 
- await seedDB();
+seedDB();
