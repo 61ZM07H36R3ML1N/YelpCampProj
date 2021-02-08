70b2ee18e0fe4f7767f2863ad49faa29b01d846d
BT <brianthomasjamiel@gmail.com>
2021-02-07 21:39:22 -0800

    added sample function for state and descriptorsd

--- seeds/index.js
+++ seeds/index.js
@@ -15,13 +15,17 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
 const seedDB = async () => {
     console.log('seeding DB');
     await Campground.deleteMany({});
     for(let i = 0; i < 100; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
-            location: '${cities[random1000].city}, ${cities[random1000].state}'
+            location: '${cities[random1000].city}, ${cities[random1000].state}',
+            title: '${sample(descriptors)} ${sample(places)}'
         })
         await camp.save();
         console.log(`Record ${i} created`);
