3bab7f89db45796f392441d5587edf3a32ffaea5
BT <brianthomasjamiel@gmaail.com>
2021-02-09 22:13:33 -0800

    added const methodOverride

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 //MongoDB connection
