7cf77fec64241664c2e1e3c9a653458bf1d507b8
BT <brianthomasjamiel@gmaail.com>
2021-03-16 11:24:33 -0700

    more code chunk

--- app.js
+++ app.js
@@ -53,12 +53,16 @@ app.post('/campgrounds', catchAsync(async (req, res, next) => {
     const campgroundSchema = Joi.object({
         campground: Joi.object({
             title: Joi.string().required(),
-            price: Joi.number().required().min(0)
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
         }).required()
     })
-    const result = campgroundSchema.validate(req.body);
-    if(result.error){
-        throw new ExpressError(result.error.details)
+    const { error } = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
     }
     console.log(result);
      const campground = new Campground(req.body.campground);
