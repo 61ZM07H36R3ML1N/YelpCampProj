930b5dfda97a00e14f4d57853aceca456e9b2259
BT <brianthomasjamiel@gmaail.com>
2021-03-17 16:56:33 -0700

    ln 36 - 52 campground validator via JOI middleware code chunk commit

--- app.js
+++ app.js
@@ -33,6 +33,23 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
+    console.log(result);
+}
 
 // App.get Routes
 app.get('/', (req, res) => {
