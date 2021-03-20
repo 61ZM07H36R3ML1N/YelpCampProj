b2e38848393c1c1da676ddd06cd16b26726279d1
BT <brianthomasjamiel@gmaail.com>
2021-03-20 16:37:20 -0700

    ln 93-100 is POST review logic

--- app.js
+++ app.js
@@ -92,7 +92,11 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) =>{
 
 app.post('/campgrounds/:id/reviews', catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
-
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 app.all('*', (req, res, next) => {
