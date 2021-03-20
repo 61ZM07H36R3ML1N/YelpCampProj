49fb5907bf1383b2fb524da59550a60f287decba
BT <brianthomasjamiel@gmaail.com>
2021-03-20 16:16:56 -0700

    removed res.send as was just making sure route was hit correctly

--- app.js
+++ app.js
@@ -89,6 +89,10 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) =>{
     res.redirect('/campgrounds');
 }))
 
+app.post('/campgrounds/:id/reviews', catchAsync(async(req, res) => {
+    
+}))
+
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
 })
