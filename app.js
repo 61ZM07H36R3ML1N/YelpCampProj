a65f65813969b5ecedd4e43fc87033a401240a95
BT <brianthomasjamiel@gmaail.com>
2021-03-20 17:50:51 -0700

    fixed typo ln 48 origin was valid which is not correct variable name. VALIDATE

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ const validateCampground = (req, res, next) => {
     console.log(result);
 }
 const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.valid(req.body);
+    const {error} = reviewSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
