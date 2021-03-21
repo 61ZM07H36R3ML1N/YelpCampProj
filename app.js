45f2dddb1c156c5b5914f71b800ab6b84a409b80
BT <brianthomasjamiel@gmaail.com>
2021-03-20 17:47:51 -0700

    added validate review logic and function to ln 102

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const {campgroundSchema} = require ('./schemas.js');
+const {campgroundSchema, reviewSchema} = require ('./schemas.js');
 const catchAsync = require('./utilities/catchAsyncError');
 const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
@@ -43,6 +43,15 @@ const validateCampground = (req, res, next) => {
         next();
     }
     console.log(result);
+}
+const validateReview = (req, res, next) => {
+    const {error} = reviewSchema.valid(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
 }
 
 // App.get Routes
@@ -90,7 +99,7 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) =>{
     res.redirect('/campgrounds');
 }))
 
-app.post('/campgrounds/:id/reviews', catchAsync(async(req, res) => {
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
