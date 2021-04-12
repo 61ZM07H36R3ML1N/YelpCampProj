bc44fbf1b1bc4935ecaf3e992f08a6c3fea5dbb5
BT <brianthomasjamiel@gmaail.com>
2021-04-12 11:38:02 -0700

    added cookie func and args for session

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const methodOverride = require('method-override');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
+const { Cookie } = require('express-session');
 
 
 //MongoDB connection
@@ -40,7 +41,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 const sessionConfig = {
     secret: 'farfignoogen',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
 }
 app.use(session(sessionConfig))
 
