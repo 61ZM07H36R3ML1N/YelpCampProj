af02495bd4a3b83ab555fd36e833a8ca33e5ae9c
BT <brianthomasjamiel@gmaail.com>
2021-05-29 12:52:43 -0700

    console.log added

--- routes/users.js
+++ routes/users.js
@@ -9,17 +9,19 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', catchAsyncError(async(req, res) => {
+router.post('/do_register', catchAsyncError(async(req, res) => {
+    console.log('post to do_register')
     try {
         const { email, username, password } =req.body;
         const user = new User({ email, username });
-        const registerUser = await User.register(user, password);    req.flash('success', 'Welcome to Yelp Camp');
+        const registerUser = await User.register(user, password); 
+        req.flash('success', 'Welcome to Yelp Camp');
         res.redirect('/campgrounds');
     } catch(e){
         req.flash('error', e.message);
         res.redirect('register');
     }
-    console.log(registerUser);
+    console.log('post do_register completed');
 
 })),
 
