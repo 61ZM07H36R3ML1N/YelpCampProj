9b56919f496680b91abce810dcf29a1b6c05336e
BT <brianthomasjamiel@gmaail.com>
2021-05-29 22:36:46 -0700

    session returnTO added

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 module.exports.isSignedIn = (req, res, next) => {
 if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl
     req.flash('error', 'you must be logged in');
     return res.redirect('/login');
     }
