47d6923a0bc4ba55fae22048bac2b900d6d14d53
BT <brianthomasjamiel@gmaail.com>
2021-05-29 22:36:29 -0700

    register USer code block

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,10 @@ router.post('/do_register', catchAsyncError(async(req, res) => {
     try {
         const { email, username, password } =req.body;
         const user = new User({ email, username });
-        const registerUser = await User.register(user, password); 
+        const registerUser = await User.register(user, password);
+        req.login(registerUser, err => {
+            if (err) return next(err);
+        }) 
         req.flash('success', 'Welcome to Yelp Camp');
         res.redirect('/campgrounds');
     } catch(e){
@@ -31,7 +34,9 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back!!!');
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo
+    res.redirect(redirectUrl);
 })
 
 router.get('/logout', (req, res) => {
